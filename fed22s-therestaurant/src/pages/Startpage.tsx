897562595704c9fb5f7511b75faa50b63873820d
coraliney <carro_gryglas@hotmail.com>
2023-06-14 12:30:36 +0200

    (feat): contact fields

--- fed22s-therestaurant/src/pages/Startpage.tsx
+++ fed22s-therestaurant/src/pages/Startpage.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import CarouselSlider from '../components/Carousel/Carousel';
-import GreenButton from '../components/GreenBtn';
-import RedButton from '../components/RedBtn';
-import NeutralButton from '../components/NeutralBtn';
-
+import { StyledDiv } from "../components/Styled/StyledDiv";
 
 
 export const Startpage = () => {
   return (
     <>
-      <p>Startpage is working</p>
-      <CarouselSlider />
-      <GreenButton></GreenButton>
-      <RedButton></RedButton>
-      <NeutralButton></NeutralButton>
+
+      <StyledDiv>
+        <p>Startpage is working</p>
+
+        <CarouselSlider />
+      </StyledDiv>
     </>
   );
 };
